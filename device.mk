bullywully/android_device_samsung_gprimelte

Reconfigure for Omni

device.mk
@@ -1,5 +1,5 @@
 #Inherit from vendor
-$(call inherit-product, vendor/samsung/gprimelte/gprimelte-vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/gprimelte/gprimelte-vendor.mk)
 
 # Inherit from common
 $(call inherit-product, device/samsung/gprimelte-common/device-common.mk)

J510-Dev/android_device_samsung_j5xnlte

device.mk
@@ -1,5 +1,5 @@
 #Inherit from vendor
-$(call inherit-product, vendor/samsung/j5/j5-vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/j53gxx/j53gxx-vendor.mk)
 
 # Inherit from common
 $(call inherit-product, device/samsung/j5-common/device-common.mk)