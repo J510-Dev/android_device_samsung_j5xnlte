bullywully/android_device_samsung_gprimelte

Reconfigure for Omni

AndroidProducts.mk
@@ -1,0 +1,3 @@
+LOCAL_PATH := device/samsung/gprimelte
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni.mk

vendorsetup.sh
@@ -1,3 +1,3 @@
 for i in eng userdebug user; do
-	add_lunch_combo lineage_gprimelte-${i}
+	add_lunch_combo omni_gprimelte-${i}
 done

J510-Dev/android_device_samsung_j5xnlte

AndroidProducts.mk
@@ -1,0 +1,3 @@
+LOCAL_PATH := device/samsung/j53gxx
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_j53gxx.mk

vendorsetup.sh
@@ -1,3 +1,3 @@
 for i in eng userdebug user; do
-	add_lunch_combo lineage_j53gxx-${i}
+	add_lunch_combo omni_j53gxx-${i}
 done