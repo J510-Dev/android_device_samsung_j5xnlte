0e309ed640e46aa7cd0921cfeb4081f9a8427cc8
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-04-12 23:08:02 -0400

    system_prop: Update product properties
    * Move screen density prop to j5-common
    * Remove EONS prop (not in stock build prop)

--- system_prop.mk
+++ system_prop.mk
@@ -7,10 +7,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.config.low_ram=false \
 	ro.config.zram=false
 
-# EONS
+# RIL
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-	persist.eons.enabled=true
-
-# Screen
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.sf.lcd_density=320
+	ro.multisim.simslotcount=2 \
+	persist.radio.multisim.config=dsds \
+	rild.libpath2=/system/lib/libsec-ril-dsds.so
